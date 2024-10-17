03afc461969e9cb0edae2031bcec55afdae717ca
behrooz bozorgchamy <behroozbc@outlook.com>
2024-10-17 22:00:39 +0200

    init charting

---
+++ draw-charts/main.py
@@ -0,0 +1,12 @@
+import json
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+dots = sns.load_dataset("dots")
+sns.relplot(
+    data=dots, kind="line",
+    x="time", y="firing_rate", col="align",
+    hue="choice", size="coherence", style="choice",
+    facet_kws=dict(sharex=False),
+)
+plt.show()
